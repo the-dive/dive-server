fb3cf4832a219bb176b309bece96b4d59e3841b8
Bibek Pandey <bewakepandey@gmail.com>
2022-11-24 14:04:10 +0545

    Add secret key to build action

--- dive/base_models.py
+++ dive/base_models.py
@@ -6,13 +6,13 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
         User,
-        related_name='%(class)s_created',
+        related_name="%(class)s_created",
         on_delete=models.CASCADE,
     )
     modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(
         User,
-        related_name='%(class)s_modified',
+        related_name="%(class)s_modified",
         on_delete=models.CASCADE,
     )
 
@@ -24,9 +24,9 @@ class NamedModelMixin(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        if hasattr(self, 'name'):
+        if hasattr(self, "name"):
             return self.name
-        return '-- No Name --'
+        return "-- No Name --"
 
     class Meta:
         abstract = True
