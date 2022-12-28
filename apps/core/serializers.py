03de1d803239670992287b38400307ae714e0b46
Bibek Pandey <bewakepandey@gmail.com>
2022-12-28 16:15:39 +0545

    Fix table properties input schema

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -6,19 +6,29 @@ from .models import Table
 from .validators import table_properties_schema
 
 
+class TablePropertiesSerializer(serializers.Serializer):
+    headerLevel = serializers.CharField()
+    timezone = serializers.CharField()
+    language = serializers.CharField()
+    trimWhitespaces = serializers.BooleanField()
+    treatTheseAsNa = serializers.CharField(required=False)
+
+    def validate(self, data):
+        if not data:
+            return data
+        try:
+            validate(data, table_properties_schema)
+        except JSONValidationError as e:
+            msg = e.message[:200] + "..." if len(e.message) > 200 else e.message
+            raise serializers.ValidationError(f"Invalid properties data: {msg}")
+        return data
+
+
 class TableUpdateSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=False)
-    properties = serializers.JSONField(required=False)
+    properties = TablePropertiesSerializer()
 
     class Meta:
         fields = '__all__'
         read_only_fields = ('status', 'preview_data', 'has_errored', 'error', 'dataset')
         model = Table
-
-    def validate_properties(self, value):
-        try:
-            validate(value, table_properties_schema)
-        except JSONValidationError as e:
-            msg = e.message[:200] + "..." if len(e.message) > 200 else e.message
-            raise serializers.ValidationError(f"Invalid properties data: {msg}")
-        return value
