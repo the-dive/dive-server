2016ead988f68064fd5e2ada644e8b4b0a46ac5f
k9845 <khatririshi2430@gmail.com>
2023-01-05 13:22:39 +0545

    Add flilter in table schema

--- apps/core/tests/test_schemas.py
+++ apps/core/tests/test_schemas.py
@@ -1,6 +1,8 @@
 from utils.graphene.tests import GraphQLTestCase
 
 from dive.consts import LANGUAGES, TABLE_HEADER_LEVELS, COLUMN_TYPES
+from apps.core.factories import TableFactory, DatasetFactory
+from apps.core.models import Table
 
 
 class GlobalPropertiesTestCase(GraphQLTestCase):
@@ -53,3 +55,56 @@ class GlobalPropertiesTestCase(GraphQLTestCase):
             len(response["data"]["propertiesOptions"]["column"]["columnTypes"]),
             len(COLUMN_TYPES),
         )
+
+
+class TableTestCase(GraphQLTestCase):
+    def test_table_filter(self):
+        query = """
+            query MyQuery($statuses: [TableStatusEnum!], $isAddedToWorkspace: Boolean) {
+                tables(statuses: $statuses, isAddedToWorkspace: $isAddedToWorkspace) {
+                    results {
+                    id
+                    name
+                    isAddedToWorkspace
+                    status
+                    }
+                }
+            }
+        """
+        dataset = DatasetFactory.create()
+        table1 = TableFactory.create(
+            is_added_to_workspace=True,
+            status=Table.TableStatus.PENDING,
+            dataset=dataset,
+        )
+        table2 = TableFactory.create(
+            is_added_to_workspace=True,
+            status=Table.TableStatus.PENDING,
+            dataset=dataset,
+        )
+        table3 = TableFactory.create(
+            is_added_to_workspace=False,
+            status=Table.TableStatus.EXTRACTED,
+            dataset=dataset,
+        )
+        table4 = TableFactory.create(
+            is_added_to_workspace=False,
+            status=Table.TableStatus.EXTRACTED,
+            dataset=dataset,
+        )
+        table5 = TableFactory.create(
+            is_added_to_workspace=True,
+            status=Table.TableStatus.PENDING,
+            dataset=dataset,
+        )
+
+        filter_data = {"isAddedToWorkspace": True}
+        content = self.query_check(query, variables={**filter_data})
+        self.assertListIds(
+            content["data"]["tables"]["results"], [table1, table2, table5]
+        )
+
+        filter_data.pop("isAddedToWorkspace")
+        filter_data = {"statuses": [self.genum(Table.TableStatus.EXTRACTED)]}
+        content = self.query_check(query, variables={**filter_data})
+        self.assertListIds(content["data"]["tables"]["results"], [table3, table4])
