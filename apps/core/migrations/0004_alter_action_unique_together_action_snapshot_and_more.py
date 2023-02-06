8a5556b119a4caed3b42e336002704552739f5bd
Bibek Pandey <bewakepandey@gmail.com>
2023-02-06 15:57:49 +0545

    Fix column stats issue

--- apps/core/migrations/0004_alter_action_unique_together_action_snapshot_and_more.py
+++ apps/core/migrations/0004_alter_action_unique_together_action_snapshot_and_more.py
@@ -5,7 +5,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("core", "0003_snapshot_remove_action_column_table_original_name_and_more"),
     ]
