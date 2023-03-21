53ddb4b1c854aebc7e4a9916ad3acc8b226891b3
k9845 <khatririshi2430@gmail.com>
2023-03-21 15:37:20 +0545

    Setup sentry

--- dive/schema.py
+++ dive/schema.py
@@ -1,7 +1,10 @@
 import graphene
+import sentry_sdk
 
+from django.conf import settings
 from apps.file import schema as file_schema
 from apps.core import schema as core_schema, mutations as core_mutations
+from graphql.execution import ExecutionResult
 
 
 # schemas
@@ -14,4 +17,21 @@ class Mutation(core_mutations.Mutation, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query, mutation=Mutation)
+class Schema(graphene.Schema):
+    def _scope_with_sentry(self, execute_func, *args, **kwargs) -> ExecutionResult:
+        if not settings.SENTRY_ENABLED:
+            return execute_func(*args, **kwargs)
+        operation_name = kwargs.get("operation_name")
+        with sentry_sdk.configure_scope() as scope:
+            scope.set_tag("kind", operation_name)
+            scope.transaction.name = operation_name
+            return execute_func(*args, **kwargs)
+
+    def execute_sync(self, *args, **kwargs) -> ExecutionResult:
+        return self._scope_with_sentry(super().execute_sync, *args, **kwargs)
+
+    def execute(self, *args, **kwargs) -> ExecutionResult:
+        return self._scope_with_sentry(super().execute, *args, **kwargs)
+
+
+schema = Schema(query=Query, mutation=Mutation)
