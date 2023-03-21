61d2a7cd7060f0180457392a4f771aa22ca69891
Bibek Pandey <bewakepandey@gmail.com>
2023-03-21 11:59:42 +0545

    Add date and datetime casts to column

--- utils/parsing.py
+++ utils/parsing.py
@@ -1,5 +1,7 @@
 from typing import Type, Any, Optional
 
+import dateparser
+
 from .common import ColumnTypes, float_r
 
 
@@ -15,6 +17,10 @@ def parse(val: Optional[Any], coltype: Any) -> str | int | float | None:
         return parse_float(strval)
     elif coltype == ColumnTypes.NUMBER:
         return parse_int(strval) or parse_float(strval)
+    elif coltype == ColumnTypes.DATE:
+        return parse_date(strval)
+    elif coltype == ColumnTypes.DATETIME:
+        return parse_datetime(strval)
     return strval
 
 
@@ -25,9 +31,27 @@ def parse_type(typ: Type, val: Optional[str]):
         return None
 
 
-def parse_int(val: Optional[str]):
+def parse_int(val: Optional[str]) -> Optional[int]:
     return parse_type(int, val)
 
 
-def parse_float(val: Optional[str]):
+def parse_float(val: Optional[str]) -> Optional[float]:
     return parse_type(float_r, val)
+
+
+def parse_datetime(val: Optional[str]) -> Optional[str]:
+    """Parse input value to datetime and return iso string if parsed"""
+    # TODO: add formatting
+    if val is None:
+        return None
+    date = dateparser.parse(val)
+    return date and date.isoformat()
+
+
+def parse_date(val: Optional[str]) -> Optional[str]:
+    """Parse input value to date and return iso string if parsed"""
+    # TODO: add formatting
+    if val is None:
+        return None
+    date = dateparser.parse(val)
+    return date and date.isoformat()[:10]
