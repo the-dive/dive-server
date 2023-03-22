b229483b06ab01ee74896b92399ee3facb5827bf
Bibek Pandey <bewakepandey@gmail.com>
2023-03-22 10:40:27 +0545

    Add gunicorn and prod script

---
+++ scripts/run_dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 manage.py migrate --no-input
+
+python3 manage.py run_celery_dev &
+python3 manage.py runserver 0.0.0.0:8000
+
--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 python3 manage.py migrate --no-input
-
-#celery -A dive worker --loglevel=info
 python3 manage.py run_celery_dev &
-python3 manage.py runserver 0.0.0.0:8000
-
+gunicorn -w 4 -b 0.0.0.0:8000 dive.wsgi:application
